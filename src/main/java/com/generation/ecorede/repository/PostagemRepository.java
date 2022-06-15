9f5ce2db90defa6568f8ce1ebf80f3f0f7e7064d
FeBelluco <felipebelluco@live.com>
2022-06-15 12:42:32 -0300

    back

---
+++ src/main/java/com/generation/ecorede/repository/PostagemRepository.java
@@ -0,0 +1,24 @@
+package com.generation.ecorede.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.generation.ecorede.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository <Postagem, Long> {
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+	
+	boolean existsById(Long id);
+
+
+	   public Postagem save(Optional<Postagem> postagem);
+	
+
+
+}
